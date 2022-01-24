1145dd077eeeb423ece5affc632704c1b3593f0d
bespalova anastasiia <anastasiia@MacBook-Pro-bespalova.local>
2022-01-24 12:23:58 +0100

    add allure Testops to login test

--- cypress/integration/loginTests.ts
+++ cypress/integration/loginTests.ts
@@ -4,6 +4,12 @@ beforeEach(function() {
 });
 
 it('Login with incorrect data', ()=> {
+    cy.allure()
+        .testID('#6922')
+        .owner('Anastasiia Bespalova')
+        .severity('critical')
+        .tag('web', 'login')
+
     cy.visit('/login')
     cy.login('email@gmail.at', 'password')
     cy.get('#message-sign-in').contains('Wrong login data.')
